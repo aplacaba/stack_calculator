bbc9a97f90cff5355c820cfe068a1e8c5c3be275
aplacaba <alacaba@fastmail.com>
2020-05-23 15:24:08 +0800

    Added unary functions

--- calc.fsx
+++ calc.fsx
@@ -16,20 +16,30 @@ let pop (StackContents contents)=
     | [] ->
         failwith "Stack underflow"
 
-let ADD stack =
-    let x,s = pop stack
-    let y,s2 = pop s
-    let result = x + y
-    push result s2
+
+let binary mathFn stack =
+    let y,stack' = pop stack
+    let x,stack'' = pop stack'
+    let z = mathFn x y
+    push z stack''
+
+let unary f stack =
+    let x,stack' = pop stack
+    push (f x) stack'
 
 
-let MUL stack =
-    let x,s = pop stack
-    let y,s2 = pop s
-    let result = x * y
-    push result s2
-    
-    
+// partial application
+let ADD = binary (+)
+let MUL = binary (*)
+let SUB = binary (-)
+let DIV = binary (/)
+let NEG = unary (fun x -> -x)
+let SQUARE = unary (fun x -> x * x)
+let SHOW stack =
+    let x,_ = pop stack
+    printfn "The answer is %f" x
+    stack
+
 let EMPTY = StackContents []
 let ONE = push 1.0
 let TWO = push 2.0
@@ -38,6 +48,12 @@ let FOUR = push 4.0
 let FIVE = push 5.0
 
 
-let add1and2 = EMPTY |> ONE |> TWO |> ADD
-let add2and3 = EMPTY |> TWO |> THREE |> ADD
-let mult2and3 = EMPTY |> TWO |> THREE |> MUL
+let div2by3 = EMPTY |> THREE |> TWO |> DIV
+let sub2from5 = EMPTY |> TWO |> FIVE |> SUB
+let add1and2thenSub3 = EMPTY |> ONE |> TWO |> ADD |> THREE |> SUB
+let neg3 = EMPTY |> THREE |> NEG
+let square2 = EMPTY |> TWO |> SQUARE
+
+printfn "%A" div2by3
+printfn "%A" neg3
+printfn "%A" square2
